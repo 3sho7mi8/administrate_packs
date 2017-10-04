2da2655a1241a6f652c023936116fcc97712020e
3sho7mi8 <hiro.pinot@gmail.com>
2017-10-04 20:45:06 +0900

    Add require to administrate/application.js

--- app/assets/javascripts/administrate-field-pell/application.js
+++ app/assets/javascripts/administrate-field-pell/application.js
@@ -1,5 +1,8 @@
 //= require jquery
 //= require jquery_ujs
+//= require selectize
+//= require moment
+//= require datetime_picker
 //= require ./pell
 //= require ./pell-content
 //= require cocoon
