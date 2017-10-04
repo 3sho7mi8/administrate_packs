af23e7b70f03b45d27d8dbc35234e274cd8da582
3sho7mi8 <hiro.pinot@gmail.com>
2017-10-04 20:15:12 +0900

    Modify pakupaku.rb

--- lib/pakupaku.rb
+++ lib/pakupaku.rb
@@ -1,8 +1,6 @@
 require 'administrate/field/paperclip'
 require 'administrate/field/nested_has_many'
 require 'administrate/field/pell'
-require "administrate/engine"
-require "rails"
 
 module Pakupaku
 end
