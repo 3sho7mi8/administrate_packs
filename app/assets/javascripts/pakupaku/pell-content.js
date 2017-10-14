9ea03f59109cc3394bf078b61f29f2cec9fc4cb1
3sho7mi8 <hiro.pinot@gmail.com>
2017-10-15 08:09:04 +0900

    Add imageUpload to Editor

--- app/assets/javascripts/pakupaku/pell-content.js
+++ app/assets/javascripts/pakupaku/pell-content.js
@@ -9,41 +9,78 @@ var _loop = function _loop(i) {
 
   pell.init({
     element: elements[i],
-    actions: ['bold', 'heading2', {
-      name: 'heading3',
-      icon: '<b>H<sub>3</sub></b>',
-      title: 'Heading 2',
-      result: function result() {
-        return pell.exec('formatBlock', '<H3>');
-      }
-    }, 'paragraph', 'olist', 'ulist', 'underline', 'strikethrough', 'italic', 'image', 'link'],
+    actions: [
+      'bold',
+      'heading2',
+      {
+        name: 'heading3',
+        icon: '<b>H<sub>3</sub></b>',
+        title: 'Heading 3',
+        result: function result() {
+          return pell.exec('formatBlock', '<H3>');
+        }
+      },
+      'paragraph',
+      'olist',
+      'ulist',
+      'underline',
+      'strikethrough',
+      'italic',
+      'link',
+      {
+        name: 'ImageUploader',
+        icon: '<label for="pell-attachment"><b>&#128247</b><sub>up</sub><input type="file" id="pell-attachment" style="display: none"></label>',
+        title: 'ImageUploader',
+        result: function result() {
+          this.getElementsByTagName('input')[0].onchange = function() {
+            var file = this.files[0]
+            var form = new FormData();
+            form.append("Content-Type", file.type);
+            form.append("editor_attachment[image]", file);
+
+            var xhr = new XMLHttpRequest;
+            xhr.open("POST", "/editor/upload.json", true);
+            xhr.setRequestHeader("X-CSRF-Token", getCsrfToken());
+
+            xhr.onload = function() {
+              if (xhr.status === 200) {
+                var data = JSON.parse(xhr.responseText);
+                document.execCommand('insertImage', false, data.link)
+              }
+            }
+            xhr.send(form)
+          }
+        }
+      },
+    ],
     root: 'pell'
   });
 
-  textAreas[i].oninput = function ($this) {
-    contents[i].innerHTML = this.value;
-    counts[i].innerHTML = this.value.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').length;
-  };
 
-  contents[i].oninput = function ($this) {
-    counts[i].innerHTML = this.textContent.length;
-    textAreas[i].value = this.innerHTML;
-  };
+textAreas[i].oninput = function ($this) {
+  contents[i].innerHTML = this.value;
+  counts[i].innerHTML = this.value.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '').length;
+};
 
-  switchs[i].onclick = function ($this) {
-    var area = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : textAreas[i];
-    var content = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : contents[i];
+contents[i].oninput = function ($this) {
+  counts[i].innerHTML = this.textContent.length;
+  textAreas[i].value = this.innerHTML;
+};
 
-    if (this.innerHTML === 'VIEW HTML') {
-      this.innerHTML = 'VIEW EDITOR';
-      area.style.width = content.clientWidth + 'px';
-      area.style.height = elements[i].clientHeight + 'px';
-      area.style.display = 'block';
-    } else {
-      this.innerHTML = 'VIEW HTML';
-      area.style.display = 'none';
-    }
-  };
+switchs[i].onclick = function ($this) {
+  var area = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : textAreas[i];
+  var content = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : contents[i];
+
+  if (this.innerHTML === 'VIEW HTML') {
+    this.innerHTML = 'VIEW EDITOR';
+    area.style.width = content.clientWidth + 'px';
+    area.style.height = elements[i].clientHeight + 'px';
+    area.style.display = 'block';
+  } else {
+    this.innerHTML = 'VIEW HTML';
+    area.style.display = 'none';
+  }
+};
 };
 
 for (var i = 0; i < elements.length; i++) {
@@ -65,6 +102,15 @@ function valueCheck() {
       contents[i].innerHTML = '<p></p>';
     }
   };
+};
+
+function getCsrfToken() {
+  var metas = document.getElementsByTagName('meta')
+  for (var i = 0; i < metas.length; i++) {
+    if (metas[i].name === "csrf-token") {
+      return metas[i].content
+    }
+  }
 };
 
 window.addEventListener("DOMContentLoaded", addTypeButton, false);
